7499ebcdfb974cac97d321bebc846a88a9ef9d4d
dlsxjzld <sinjuk1@naver.com>
2024-12-12 14:25:07 +0900

    feat: 게임 시작 문구 출력 및 다리 길이 입력 연결

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import BridgeMaker from './BridgeMaker.js';
+import { InputView } from './view/InputView.js';
+import { OutputView } from './view/OutputView.js';
+import { validateSomething } from './validation/validateFunctions.js';
+
+import { MESSAGE } from './constants/message.js';
 
 class App {
-  play() {
-    console.log(typeof BridgeMaker.makeBridge);
+  async play() {
+    OutputView.printGameInstruction();
+    const bridgeSize = this.getBridgeSize();
+  }
+
+  async getBridgeSize() {
+    try {
+      const input = await InputView.readBridgeSize();
+      validateSomething(input);
+      return input;
+    } catch (error) {
+      OutputView.printResult(error.message);
+      return this.getBridgeSize();
+    }
   }
 }
 
