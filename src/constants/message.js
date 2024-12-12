108aaa264d5bd9b4da7de29ab405cf01480d1270
dlsxjzld <sinjuk1@naver.com>
2024-12-12 16:40:46 +0900

    refactor: 출력 형식에 맞게 메시지 상수 수정

--- src/constants/message.js
+++ src/constants/message.js
@@ -4,7 +4,7 @@ export const MESSAGE = {
   ASK_MOVE: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
   ASK_GAME_TRY:
     '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
-  GAME_RESULT_INSTRUCTION: '최종 게임 결과\n',
+  GAME_RESULT_INSTRUCTION: '최종 게임 결과',
   GAME_SUCCESS_RESULT: (result) => `게임 성공 여부: ${result}`,
   GAME_TRY_COUNT: (result) => `총 시도한 횟수: ${result}`,
 };
