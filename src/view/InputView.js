eb9b0e6a3a8ae48d639454cd6777b87b4b7bb392
dlsxjzld <sinjuk1@naver.com>
2024-12-12 14:24:44 +0900

    feat: 다리 길이 입력 받기 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,4 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
+import { MESSAGE } from '../constants/message.js';
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -7,7 +8,10 @@ export const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  async readBridgeSize() {
+    const input = await Console.readLineAsync(MESSAGE.ASK_BRIDGE_LENGTH);
+    return input;
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
