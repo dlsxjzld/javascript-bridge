f7aa3fa40f2989bf033e562e9ba7f9dc015d8814
dlsxjzld <sinjuk1@naver.com>
2024-12-12 15:09:51 +0900

    feat: 컨트롤러에 총 시도 횟수 추가

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import { validateBridgeSize } from './validation/validateFunctions.js';
 
 import { MESSAGE } from './constants/message.js';
 import BridgeRandomNumberGenerator from './model/BridgeRandomNumberGenerator.js';
+import BridgeGame from './model/BridgeGame.js';
 
 class App {
+  gameCount = 0; // 총 시도 횟수
+
   async play() {
     OutputView.printGameInstruction();
     const bridgeSize = await this.getBridgeSize();
@@ -14,6 +17,8 @@ class App {
     const totalBridge = Array.from({ length: 2 }, () =>
       BridgeMaker.makeBridge(bridgeSize, BridgeRandomNumberGenerator.generate),
     );
+    // TODO: 게임 시작하고 브릿지 게임 실패하면 R 혹은 Q 입력
+    this.startBridgeGame(totalBridge);
   }
 
   // console.log(
@@ -31,6 +36,12 @@ class App {
       OutputView.printResult(error.message);
       return this.getBridgeSize();
     }
+  }
+
+  async startBridgeGame(bridge) {
+    this.gameCount += 1;
+    const bridgeGame = new BridgeGame(bridge);
+    // TODO: 매 라운드가 성공했는지 실패했는지 반환 받고 이걸 토대로 startBridgeGame을 다시 할지 정함
   }
 }
 
