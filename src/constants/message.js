275a9c41e0934f59cdff7f8578878c419e940db3
dlsxjzld <sinjuk1@naver.com>
2024-12-12 14:16:40 +0900

    feat: 입출력 메시지 상수화 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,9 +1,14 @@
 export const MESSAGE = {
-  ASK_MONTH_DAY: '비상 근무를 배정할 월과 시작 요일을 입력하세요> ',
-  ASK_WEEKDAY_MAN: '평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ',
-  ASK_HOLIDAY_MAN: '휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ',
+  GAME_INSTRUCTION: '다리 건너기 게임을 시작합니다.\n',
+  ASK_BRIDGE_LENGTH: '다리의 길이를 입력해주세요.\n',
+  ASK_MOVE: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  ASK_GAME_TRY:
+    '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+  GAME_RESULT_INSTRUCTION: '최종 게임 결과\n',
+  GAME_SUCCESS_RESULT: (result) => `게임 성공 여부: ${result}`,
+  GAME_TRY_COUNT: (result) => `총 시도한 횟수: ${result}`,
 };
 
 export const ERROR_MESSAGE = {
-  INVALID: '유효하지 않은 입력 값입니다. 다시 입력해 주세요.',
+  INVALID: '다리 길이는 3부터 20 사이의 숫자여야 합니다.',
 };
